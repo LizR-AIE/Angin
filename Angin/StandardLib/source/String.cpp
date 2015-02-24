18405eaa5b4ae628a769eb34ebd46b97100f32c0
LizR-AIE <lizr@aie.edu.au>
2015-02-25 08:47:51 +1100

    more string operators

--- Angin/StandardLib/source/String.cpp
+++ Angin/StandardLib/source/String.cpp
@@ -12,13 +12,47 @@ String::String(char * a_string)
 	UpdateLength();
 }
 
+String::String(const String & a_other)
+{
+	m_length = a_other.GetLength();
+	m_charArray = new char[m_length + 1];
+
+	for (unsigned int i = 0; i < m_length; i++)
+	{
+		// Copy over the data from the other string into this string
+		m_charArray[i] = a_other.m_charArray[i];
+	}
+	// add a null turminator to the end of the char array
+	m_charArray[m_length + 1] = 0;
+}
+
+String::String(String && a_other)
+{
+	// move across the length of the string
+	m_length = a_other.m_length;
+	// create memory that is long enough for the string plus a null terminator				
+	m_charArray = new char[m_length + 1]{0};
+	// copy over the data from the other string
+	for (unsigned int i = 0; i < m_length; i++)
+	{
+		m_charArray[i] = a_other.m_charArray[i];
+	}
+	// add the null terminator
+	m_charArray[m_length] = 0;
+	// free the memory used for the other string
+	delete[] a_other.m_charArray;
+	// make the memory null
+	a_other.m_charArray = nullptr;
+}
+
 String::~String()
 {
 	if (m_charArray != nullptr)
 	{
 		delete[] m_charArray;
-		m_length = 0;
+		m_charArray = nullptr;
 	}
+	m_length = 0;
 }
 
 String & String::operator=(const String & a_other)
@@ -33,13 +67,15 @@ String & String::operator=(const String & a_other)
 		// Copy over the data from the other string into this string
 		m_charArray[i] = a_other.m_charArray[i];
 	}
-	
+	// add a null turminator to the end of the char array
+	m_charArray[m_length + 1] = 0;
+
 	return *this;
 }
 
 String & String::operator=(String && a_other)
 {
-	//if (this != a_other)
+	if (*this != a_other)
 	{
 		// free out current string
 		delete[] m_charArray;
@@ -48,7 +84,7 @@ String & String::operator=(String && a_other)
 		// create memory that is long enough for the string plus a null terminator				
 		m_charArray = new char[m_length + 1]{0};
 		// copy over the data from the other string
-		for (int i = 0; i < m_length; i++)
+		for (unsigned int i = 0; i < m_length; i++)
 		{
 			m_charArray[i] = a_other.m_charArray[i];
 		}
@@ -60,6 +96,54 @@ String & String::operator=(String && a_other)
 		a_other.m_charArray = nullptr;
 	}
 	return *this;
+}
+
+String String::operator+(const String & a_other) const
+{
+	// Create the string to return
+	String temp;
+	// Calculate the length of the new string
+	temp.m_length = m_length + a_other.m_length;
+	// Allocate memory for the string to be stored in
+	temp.m_charArray = new char[m_length + 1];
+	// copy the string from this to the new string
+	for (unsigned int i = 0; i < m_length; i++)
+	{
+		temp.m_charArray[i] = m_charArray[i];
+	}
+	// copy the string from the other string into the new string
+	for (unsigned int i = m_length; i < temp.m_length; i++)
+	{
+		temp.m_charArray[i] = a_other.m_charArray[i];
+	}
+	// add a null terminator to the end
+	temp.m_charArray[temp.m_length + 1] = 0;
+
+	return temp;
+}
+
+String & String::operator+=(const String & a_other)
+{
+	return *this = *this + a_other;
+}
+
+bool String::operator==(const String & a_other)
+{
+	if (m_length != a_other.GetLength())
+		return false;
+
+	for (unsigned int i = 0; i < m_length; i++)
+	{
+		if (m_charArray[i] != a_other.m_charArray[i])
+			return false;
+	}
+
+	return true;
+}
+
+bool String::operator!=(const String & a_other)
+{
+	return !(*this == a_other);
 }
 
 const unsigned int String::GetLength() const
--- Angin/StandardLib/source/String.h
+++ Angin/StandardLib/source/String.h
@@ -7,19 +7,29 @@ public:
 	String();
 	// Constructor that takes in a char array
 	String(char * string);
+	// Copy constructor
+	String(const String & other);
+	// Move constructor
+	String(String && other);
 	// Destructor
 	~String();
 	// Copy assignment: leaves a copy there
 	String & operator = (const String & other);
 	// Move assignment: moves the data from other
 	String & operator = (String && other);
-
-	//Returns the current length of the String
+	// + operator: returns a string that contains the two strings
+	String operator + (const String & other) const;
+	// += operator: adds the other string to this string
+	String & operator += (const String & other);
+	// == operator: returns true if the two strings are the same
+	bool operator == (const String & other);
+	// != operator: returns true if the two strings are different
+	bool operator != (const String & other);
+	// Returns the current length of the String
 	const unsigned int GetLength() const;
-	
 	// Returns the begining of the string as a char reference
 	char & GetString() const;
-	
+	// Recalculate the length of the string
 	void UpdateLength();
 		
 private:
